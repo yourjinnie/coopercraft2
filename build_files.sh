68a2bf7dad4c139552349bb72f6686e628721405
Ashim Mistry <ashim.anuvab@gmail.com>
2024-06-25 20:39:14 +0530

    My second commit

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,17 @@
-echo "BUILD START"
-python-3.12.1 -m pip install -r requirment.txt
-ppython-3.12.1 manage.py collectstatic --noinput --clear
-echo "BUILD END"
+#echo "BUILD START"
+#python-3.12.1 -m pip install -r requirment.txt
+#ppython-3.12.1 manage.py collectstatic --noinput --clear
+#echo "BUILD END"
+#!/bin/sh
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Apply database migrations
+python manage.py migrate
