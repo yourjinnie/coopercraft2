999de3463797bd00a322952130f5e028fb9d70fc
Ashim Mistry <ashim.anuvab@gmail.com>
2024-06-25 20:34:45 +0530

    My second commit

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo "BUILD START"
-python3.12.1 -m pip install -r requirment.txt
-ppython3.12.1 manage.py collectstatic --noinput --clear
+python-3.12.1 -m pip install -r requirment.txt
+ppython-3.12.1 manage.py collectstatic --noinput --clear
 echo "BUILD END"
